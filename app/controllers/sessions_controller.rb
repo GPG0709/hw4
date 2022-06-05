0314c4842825fca9cc070a783d2ff49b49ab5c26
GPG0709 <guillermo.puga@kellogg.northwestern.edu>
2022-06-05 19:43:57 +0000

    v2

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,26 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user = User.find_by({ "email" => params["email"] })
+    if @user
+      if BCrypt::Password.new(@user["password"]) == params["password"]
+        session["user_id"] = @user["id"]
+        flash["notice"] = "Hello."
+        redirect_to "/places"
+      else
+        flash["notice"] = "Nope."
+        redirect_to "/login"
+      end
+    else
+      flash["notice"] = "Nope."
+      redirect_to "/login"
+    end
   end
 
   def destroy
+    session["user_id"] = nil
+    flash["notice"] = "See you later, Aligator"
+    redirect_to "/login"
   end
 end
   
